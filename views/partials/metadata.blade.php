8d1989a8bfb1bc478a018a734e5f6d6939175d8a
themelogy <ismailsagsoz@gmail.com>
2017-04-04 03:21:10 +0200

    updating metadata and scripts

--- views/partials/metadata.blade.php
+++ views/partials/metadata.blade.php
@@ -4,20 +4,4 @@
 <!--[if IE]>
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <![endif]-->
-<meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="icon" type="image/png" href="{{ Theme::url('images/favicon.ico') }}">
-
-{!! Theme::style('css/bootstrap.min.css') !!}
-{!! Theme::style('css/main.min.css') !!}
-{!! Theme::style('css/animations.css') !!}
-{!! Theme::style('css/fonts.css') !!}
-{!! Theme::style('vendor/layerslider/css/layerslider.css') !!}
-{!! Theme::style('vendor/flag-icon-css/css/flag-icon.min.css') !!}
-@stack('styles')
-
-{!! Theme::script('js/vendor/modernizr-2.6.2.min.js') !!}
-<!--[if lt IE 9]>
-{!! Theme::script('js/vendor/html5shiv.min.js') !!}
-{!! Theme::script('js/vendor/respond.min.js') !!}
-{!! Theme::script('js/vendor/jquery-1.12.4.min.js') !!}
-<![endif]-->
