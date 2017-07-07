b98c59f81c2fac1f3fa134309d878b94ea3c16ca
themelogy <ismailsagsoz@gmail.com>
2017-07-07 23:01:03 +0300

    Updating Theme

---
+++ views/modules/page/tag.blade.php
@@ -0,0 +1,32 @@
+@extends('layouts.master')
+
+@section('breadcrumbs')
+    @include('partials.parts.breadcrumbs', ['title'=>trans('tag::tags.tag') . ' : ' . $tag->name, 'breadcrumbs'=>'page.tag'])
+@endsection
+
+@section('content')
+    <section class="ls section_padding_50">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <div class="row">
+                        @if(count($pages)>0)
+                            @foreach($pages as $page)
+                                <div class="col-sm-6 col-md-4">
+                                    <div class="thumbnail">
+                                        <img class="img-thumbnail" src="{{ $page->present()->firstImage(null, 250, 'resize', 80) }}" alt="{{ $page->title }}" />
+                                        <div class="caption">
+                                            <h3>{{ $page->title }}</h3>
+                                            <p>{{ strip_tags(str_sentence($page->body, 1)) }}</p>
+                                            <p><a href="{{ $page->url }}" class="btn btn-default" role="button">{{ trans('global.buttons.read more') }}</a>
+                                        </div>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @endif
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@stop
